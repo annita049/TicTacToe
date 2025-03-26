939eb067e1e54b0e4ae4a745b6518d6dbb15785d
Annita <e.annita2001@gmail.com>
2025-03-26 20:06:14 +0600

    with game-result component

---
+++ src/components/GameOver.jsx
@@ -0,0 +1,9 @@
+const GameOver = () => {
+  return (
+    <div className="game-over">
+        GameOver
+    </div>
+  )
+}
+
+export default GameOver
--- src/components/TicTacToe.jsx
+++ src/components/TicTacToe.jsx
@@ -1,6 +1,8 @@
 import Board from './Board';
 import { useState, useEffect } from 'react';
 
+import GameOver from './GameOver';
+
 const player_X = 'X';
 const player_O = 'O';
 
@@ -9,13 +11,29 @@ const WinningCombinations = [  // array of objects
   {combo: [3,4,5], strikeClass: "strike-row-2"},
   {combo: [6,7,8], strikeClass: "strike-row-3"},
 
-  {combo: [0,3,6], strikeClass: "strike-col-1"},
-  {combo: [1,4,7], strikeClass: "strike-col-2"},
-  {combo: [2,5,8], strikeClass: "strike-col-3"},
+  {combo: [0,3,6], strikeClass: "strike-column-1"},
+  {combo: [1,4,7], strikeClass: "strike-column-2"},
+  {combo: [2,5,8], strikeClass: "strike-column-3"},
 
   {combo: [0,4,8], strikeClass: "strike-diagonal-1"},
   {combo: [2,4,6], strikeClass: "strike-diagonal-2"}
 ]
+
+const checkWinner = (tiles, setstrikeClass, playerTurn)=>{
+  console.log("check winner?");
+  console.log(tiles);
+  for(let {combo, strikeClass} of WinningCombinations){
+    const tile1 = tiles[combo[0]];
+    const tile2 = tiles[combo[1]];
+    const tile3 = tiles[combo[2]];
+    
+    if(tile1!=null && tile1 == tile2 && tile2 == tile3 && tile1 == tile3){
+      console.log(`${playerTurn} wins!`);
+      console.log("k",combo[0]," ",combo[1]," ",combo[2]);
+      setstrikeClass(strikeClass);
+    }
+  }
+}
 
 
 const TicTacToe = () => {
@@ -23,23 +41,6 @@ const TicTacToe = () => {
   const [playerTurn, setPlayerTurn] = useState(player_X);
   const [strikeClass, setstrikeClass] = useState();
 
-
-  
-const checkWinner = ()=>{
-  console.log("check winner?");
-  for(let {combo, strikeClass} of WinningCombinations){
-    const tile1 = tiles[combo[0]];
-    const tile2 = tiles[combo[1]];
-    const tile3 = tiles[combo[2]];
-
-    
-    if(tile1 == tile2 && tile2 == tile3 && tile1 == tile3){
-      console.log(`${playerTurn} wins!`);
-      setstrikeClass(strikeClass);
-    }
-
-  }
-}
 
   const handleTileClick = (index)=>{ //index of the tile clicked
     if(tiles[index]) return;
@@ -54,13 +55,15 @@ const checkWinner = ()=>{
   }
 
   useEffect(()=>{
-    checkWinner();
-  },[tiles]);
+    checkWinner(tiles, setstrikeClass, playerTurn);
+  },[tiles, playerTurn]);
+
 
   return (
     <div>
       <h1>TicTacToe</h1>
       <Board strike={strikeClass} playerTurn={playerTurn} tiles={tiles} onTileClick={handleTileClick}/>
+      <GameOver/>
     </div>
   )
 }
