5967b28a225f454aec10933e3bf5bd131a3dd607
Annita <e.annita2001@gmail.com>
2025-03-26 15:25:42 +0600

    restricts a player to play twice on a tile

--- src/components/TicTacToe.jsx
+++ src/components/TicTacToe.jsx
@@ -2,16 +2,22 @@ import Board from './Board';
 import { useState } from 'react';
 
 const player_X = 'X';
-const player_Y = 'Y';
+const player_O = 'O';
 
 const TicTacToe = () => {
   const [tiles, setTiles] = useState(Array(9).fill(null));
   const [playerTurn, setPlayerTurn] = useState(player_X);
 
   const handleTileClick = (index)=>{ //index of the tile clicked
-    const newTiles = [...tiles];
+    if(tiles[index]) return;
+    
+    const newTiles = [...tiles]; // a new array is taken cause
+    // state updates are supposed to be immutable in react
     newTiles[index] = playerTurn; 
     setTiles(newTiles);  // updates tiles with players turn 'O' or 'X'
+
+    playerTurn =='X'? setPlayerTurn(player_O) : setPlayerTurn(player_X);
+
   }
 
   return (
